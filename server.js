16cd6acbb70e2fbe0bca644a2ace00871819f3f1
Tu Hoang <hatu87@gmail.com>
2016-05-03 17:13:42 +0700

    Finish say hello feature

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+var ect = require('ect');
+var renderer = ect({
+  root: __dirname + 'views',
+  ext: '.ect'
+});
+
+app.set('view engine', 'ect');
+app.engine('ect', renderer.render);
+
 app.get('/hello', function(req, res) {
-  res.send('<html><head></head><body><form><input name="name"/> <button type="submit">Say Hello</button></form><h1>Hello, ' + req.query.name + '</h1></body></html>');
+  // res.send('<html><head></head><body><form><input name="name"/> <button type="submit">Say Hello</button></form><h1>Hello, ' + req.query.name + '</h1></body></html>');
+  res.render('hello', { name: req.query.name });
 });
 
 var port = 3000;
