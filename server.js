ae54aa3681719181898f98299f8f213773426d11
Tu Hoang <hatu87@gmail.com>
2016-05-03 16:13:38 +0700

    Add form hello

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res) {
-  res.send('Hello, ' + req.query.name);
+  res.send('<html><head></head><body><form><input name="name"/> <button type="submit">Say Hello</button></form><h1>Hello, ' + req.query.name + '</h1></body></html>');
 });
 
 var port = 3000;
